890bc429a14b1f9ab443a7effa39a317b4c77435
G <gregor@gregorfra.me>
2019-10-31 15:43:53 +0000

    Commented code, removed always-random

--- logic.js
+++ logic.js
@@ -1,12 +1,21 @@
-// Create 16x16 divs
+// Defines the initial color
 let whichColor = "blue";
+
+// Defines each of the buttons
 const divContainer = document.getElementById("divContainer");
-const colorPicker = document.getElementById("color-pick").addEventListener("click", userColor);
-const randomColor = document.getElementById("random-color").addEventListener("click", getRandomColor);
-//const totalRandom = document.getElementById("always-random-color").addEventListener("click", alwaysRandom);
+const colorPicker = document
+  .getElementById("color-pick")
+  .addEventListener("click", userColor);
+const randomColor = document
+  .getElementById("random-color")
+  .addEventListener("click", getRandomColor);
+/*const totalRandom = document
+  .getElementById("always-random-color")
+  .addEventListener("change", alwaysRandom);
+*/
 
-// const button = document.getElementById("btn").addEventListener("click", generateGrid);
 
+// Create the initial 16x16 divs
 function initialGrid() {
   let numberOfSquares = 16 * 16;
   for (let i = 0; i < numberOfSquares; i++) {
@@ -17,13 +26,14 @@ function initialGrid() {
       "grid-template-columns: repeat(16, 1fr); grid-template-rows: repeat(16, 1fr);"
     );
     divContainer.appendChild(divGrid);
-    unRandom();
     changeBackground(whichColor);
   }
 }
 
 initialGrid();
 
+
+// Clears the existing grid and creates a new grid defined by the user
 let clear = document
   .getElementById("clear-grid")
   .addEventListener("click", generateGrid);
@@ -45,12 +55,19 @@ function generateGrid() {
         "repeat(" + usersChoice + ", 1fr)";
       divContainer.style.gridTemplateRows = "repeat(" + usersChoice + ", 1fr)";
       divContainer.appendChild(divGrid);
-      unRandom();
       changeBackground(whichColor);
     }
   }
 }
+// Function to clear the grid
+function clearGrid() {
+    let toRemove = document.getElementsByClassName("grid");
+    while (toRemove.length > 0) {
+      toRemove[0].parentNode.removeChild(toRemove[0]);
+    }
+  }
 
+// Colors the background of the divs
 function changeBackground(whichColor) {
   [...document.querySelectorAll(".grid")].forEach(grid => {
     grid.addEventListener("mouseover", function() {
@@ -59,37 +76,21 @@ function changeBackground(whichColor) {
   });
 }
 
-function clearGrid() {
-  let toRemove = document.getElementsByClassName("grid");
-  while (toRemove.length > 0) {
-    toRemove[0].parentNode.removeChild(toRemove[0]);
-  }
+
+// Allows the user to select their own color
+function userColor() {
+  changeColor = prompt(
+    "Which color would you like to use? (e.g. Blue, #000CFF, rgb(0, 12, 255)"
+  );
+  changeBackground(changeColor);
 }
 
-function userColor () {
-    changeColor = prompt("Which color would you like to use? (e.g. Blue, #000CFF, rgb(0, 12, 255)");
-    changeBackground(changeColor);
-}
-
+// Gets a random color
 function getRandomColor() {
-    let letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    changeBackground(color);
+  let letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
   }
-
-/*function alwaysRandom (){
-    if (document.getElementById("always-random-color").checked) {
-        [...document.querySelectorAll(".grid")].forEach(grid => {
-            grid.addEventListener("mouseover", function() {
-              grid.style.backgroundColor = getRandomColor();
-            });
-          });
-        }
-    }*/
-
-function unRandom () {
-    document.getElementById("always-random-color").checked = false;
+  changeBackground(color);
 }
